544a1c1ff27f26b2a7b61b825ab36b56282c579c
Julian Doherty (madlep) <madlep@madlep.com>
2013-06-01 21:45:07 +1000

    fix silly missing newline

--- ruby/spec/algorithm/quicksort_spec.rb
+++ ruby/spec/algorithm/quicksort_spec.rb
@@ -1,6 +1,7 @@
 require "algorithm/quicksort"
 
-describe "#quicksort" do let(:list) { [6,2,8,3,4,1,9,0,5,7] }
+describe "#quicksort" do
+  let(:list) { [6,2,8,3,4,1,9,0,5,7] }
   let(:sorted_list) { [0,1,2,3,4,5,6,7,8,9] }
 
   it "should sort a list" do
