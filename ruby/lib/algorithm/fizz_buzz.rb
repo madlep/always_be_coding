f210548c40401749bc87e344b9d35e23d4c04b8c
Julian Doherty (madlep) <madlep@madlep.com>
2013-06-01 21:51:24 +1000

    cleverised fizz_buzz

--- ruby/lib/algorithm/fizz_buzz.rb
+++ ruby/lib/algorithm/fizz_buzz.rb
@@ -1,15 +1,9 @@
 module Algorithm
   def self.fizz_buzz
     (1..100).map {|n|
-      if n % 3 == 0 && n % 5 == 0
-        "FizzBuzz"
-      elsif n % 3 == 0
-        "Fizz"
-      elsif n % 5 == 0
-        "Buzz"
-      else
-        n.to_s
-      end
+      fizz = n % 3 == 0 ? "Fizz" : nil
+      buzz = n % 5 == 0 ? "Buzz" : nil
+      fizz || buzz ? "#{fizz}#{buzz}" : n.to_s
     }
   end
 end
